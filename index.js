e4d83b50d94d2b646b98d9acf3dc71f9318f01c8
전병길 <jeonbg@kakao.com>
2022-03-16 21:51:57 +0900

    #0.4 : First steps with Typescript

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Genie", age = 3, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you ar a ${gender}`);
+};
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "Genie",
+  age = 3,
+  gender = "male";
+
+const sayHi = <S, N>(name: S, age: N, gender: S) => {
+  console.log(`Hello ${name}, you are ${age}, you ar a ${gender}`);
+};
+
+sayHi<string, number>(name, age, gender);
+
+export {};
