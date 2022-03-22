27be5f78b0677c2ce5bd8f343081b6abbc3b1189
전병길 <jeonbg@kakao.com>
2022-03-22 22:29:12 +0900

    Creating a Block part Two

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import * as CrpytoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+  // static 으로 선언된 변수, 함수는 생성자로 생성하지 않고 호출하여 사용할 수 있다.
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CrpytoJS.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(
     index: number,
     hash: string,
@@ -22,5 +33,11 @@ class Block {
 const genesisBlock: Block = new Block(0, "230230230320", "", "Hello", 123456);
 
 let blockChain: Block[] = [genesisBlock];
+
+const getBlockChain = (): Block[] => blockChain;
+
+const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
