ff3ecfe78f2f8513defb72c1db04ce52c71c7b82
전병길 <jeonbg@kakao.com>
2022-03-22 22:14:19 +0900

    #0.7 Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  // private 로 선언하면 내부에서만 사용 가능하다. ( 바깥에서는 접근하지 못한다. )
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "jb",
-  age: 10,
-  gender: "male",
-};
+const lynn = new Human("Lynn", 18, "female");
 
 const sayHi = (person: Human): string => {
   const { name, age, gender } = person;
   return `Hello ${name}, you are ${age}, you ar a ${gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};
