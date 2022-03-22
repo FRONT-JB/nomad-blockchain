e78291ff12e690d0869ca3693533eb7639e3f228
전병길 <jeonbg@kakao.com>
2022-03-22 22:08:05 +0900

    #0.6 Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-const sayHi = <S, N>(name: S, age: N, gender: S): string => {
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "jb",
+  age: 10,
+  gender: "male",
+};
+
+const sayHi = (person: Human): string => {
+  const { name, age, gender } = person;
   return `Hello ${name}, you are ${age}, you ar a ${gender}`;
 };
 
-console.log(sayHi<string, number>("Genie", 50, "Male"));
+console.log(sayHi(person));
 
 export {};
