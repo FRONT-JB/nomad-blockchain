feed4f296348a6ae4f659988de08e055786052e5
전병길 <jeonbg@kakao.com>
2022-03-22 22:20:46 +0900

    #0.8 Blockchain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  // private 로 선언하면 내부에서만 사용 가능하다. ( 바깥에서는 접근하지 못한다. )
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const lynn = new Human("Lynn", 18, "female");
+const genesisBlock: Block = new Block(0, "230230230320", "", "Hello", 123456);
 
-const sayHi = (person: Human): string => {
-  const { name, age, gender } = person;
-  return `Hello ${name}, you are ${age}, you ar a ${gender}`;
-};
-
-console.log(sayHi(lynn));
+let blockChain: Block[] = [genesisBlock];
 
 export {};
